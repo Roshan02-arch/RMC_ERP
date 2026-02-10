b6c4478c059722447229491f146b0751598959a9
Akshaya Achariya <akshayaachariya0403@gmail.com>
2026-02-10 12:54:02 +0530

    commit spring initializr folder

---
+++ backend/demo/src/main/java/com/demo/RmcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmcBackendApplication.class, args);
+	}
+
+}
